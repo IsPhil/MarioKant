f29aed294bb092e26b686ad6620b0b7f9d2609fa
Phil0411 <philka0411@gmail.com>
2020-02-10 00:24:09 +0100

    Projekt erstellt

--- MarioKant/src/Main.cpp
+++ MarioKant/src/Main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Font.hpp>
 
 int main() {
-	std::cout << "Shit is working!" << std::endl;
-	std::cin.get();
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Ass Link"); //Ass Link
+	sf::Font font;
+	
+	if (!font.loadFromFile("res/fonts/dacasa.ttf")) {
+		std::cout << "Fuck" << std::endl;
+		return -1;
+	}
+	
+	sf::Text text("Ass Link", font, 40U);
+	text.setStyle(sf::Text::Bold);
+
+	while (window.isOpen()) {
+		sf::Event event;
+
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(text);
+		window.display();
+	}
 	
 	return 0;
 }
