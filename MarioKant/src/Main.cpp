430748d749a24e7a389d3504ab3b37510ec19028
Phil0411 <philka0411@gmail.com>
2020-02-10 01:21:39 +0100

    Anfang der Engine

--- MarioKant/src/Main.cpp
+++ MarioKant/src/Main.cpp
@@ -1,22 +1,9 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
+#include "Game.h"
+
+Game game;
 
 int main() {
-	const char* TITLE = "Ass Link"; //Ass Link
-	
-	sf::RenderWindow window(sf::VideoMode().getDesktopMode(), TITLE, sf::Style::Fullscreen);
-	
-	while (window.isOpen()) {
-		sf::Event event;
+	game.run();
 
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
-
-		window.clear();
-		window.display();
-	}
-	
 	return 0;
 }
